bd8d6b07d6c60402798fdc788a7591e0e0160f1d
Hussain Ahmed <hussainahmed.bscsf23khp@iba-suk.edu.pk>
2024-10-08 12:25:55 +0500

    Update Main.java

--- LABs/Lab1/src/Main.java
+++ LABs/Lab1/src/Main.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //hey this is hussain ahmed
+        // hey this is Hussain ahmed
+        // this is for 3rd commit
 
         // Take input the size of an array
-        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        Scanner sc = new Scanner(System.in); // Create a Scanner object
         System.out.println("Enter the size of an Array");
-        int size = sc.nextInt();  // Read user input
+        int size = sc.nextInt(); // Read user input
 
-        //create array with the input size on random value
+        // create array with the input size on random value
         Random rd = new Random(); // creating Random object
         rd.setSeed(System.currentTimeMillis()); // use current time as a seed
         // fill the data array with pseudo-random numbers from 0 to 99,
@@ -19,11 +20,10 @@ public class Main {
         for (int i = 0; i < arr.length; i++)
             arr[i] = rd.nextInt(100); // storing random integers in an array
 
-
-        //Display the defined array
+        // Display the defined array
         System.out.println(Arrays.toString(arr));
 
-        //delete an element from array
+        // delete an element from array
         arr = MyArray.removeTheElement(arr, 2);
         System.out.println(Arrays.toString(arr));
     }
